90648dcad0197c58117ab9abde2cad98cb024407
joelmarcolino <jjoelmarcolinomoraisjoao@gmail.com>
2023-01-09 22:48:26 +0000

    adicionar styled-components

---
+++ components/Header/index.tsx
@@ -0,0 +1,9 @@
+import * as S from './styles';
+
+const Header=()=>{
+    return(
+        <S.Container>
+        
+        </S.Container>
+    )
+}
--- pages/index.tsx
+++ pages/index.tsx
@@ -1,7 +1,4 @@
 import Head from 'next/head'  
-import { Inter } from '@next/font/google'
-
-const inter = Inter({ subsets: ['latin'] })
 
 export default function Home() {
   return (
@@ -13,7 +10,7 @@ export default function Home() {
         <link rel="icon" href="/favicon.ico" />
       </Head>
       <main>
-    .
+        
       </main>
     </>
   )
